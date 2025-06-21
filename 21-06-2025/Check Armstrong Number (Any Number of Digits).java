6102034f858a981b39c2b9076e0744b5aceaa559
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-21 20:12:57 +0530

    Create Check Armstrong Number (Any Number of Digits).java

---
+++ 21-06-2025/Check Armstrong Number (Any Number of Digits).java
@@ -0,0 +1,14 @@
+
+
+public class GeneralArmstrong {
+    public static void main(String[] args) {
+        int num = 9474, temp = num, sum = 0;
+        int digits = String.valueOf(num).length();
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+        System.out.println(num + " is " + (num == sum ? "Armstrong" : "Not Armstrong"));
+    }
+}
