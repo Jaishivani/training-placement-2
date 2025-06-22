538a4a74f8f035b9e8aaa7c25a8405e18349ee24
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-22 19:50:52 +0530

    Create Convert String to Integer.java

---
+++ 22-06-2025/Convert String to Integer.java
@@ -0,0 +1,8 @@
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println("Integer: " + num);
+    }
+}
