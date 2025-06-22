f70496ded4203787d2764feb35beb7f557240abe
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-22 19:49:27 +0530

    Create Check Whether a Character is Vowel or Consonant.java

---
+++ 22-06-2025/Check Whether a Character is Vowel or Consonant.java
@@ -0,0 +1,10 @@
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        char ch = 'e';
+        if ("aeiouAEIOU".indexOf(ch) != -1)
+            System.out.println(ch + " is a Vowel");
+        else
+            System.out.println(ch + " is a Consonant");
+    }
+}
