ee94031b0769d51dc1bb628bb71e60308cda35f6
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-22 19:53:42 +0530

    Create Convert Integer to String.java

---
+++ 22-06-2025/Convert Integer to String.java
@@ -0,0 +1,9 @@
+
+
+public class IntToString {
+    public static void main(String[] args) {
+        int num = 456;
+        String str = Integer.toString(num);
+        System.out.println("String: " + str);
+    }
+}
