4c38cd3d7566dff8dc7366ef19daf2204f488ab8
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-22 19:55:56 +0530

    Create Java Program to Display Current Date and Time.java

---
+++ 22-06-2025/Java Program to Display Current Date and Time.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeNow {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println("Current Date and Time: " + now.format(format));
+    }
+}
