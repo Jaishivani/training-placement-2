4b90b33b729721c702984ddbe73e453dfc63c287
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2025-06-23 12:26:13 +0530

    Create Check if Two Strings are Rotations.java

---
+++ 23-06-2025/Check if Two Strings are Rotations.java
@@ -0,0 +1,11 @@
+
+
+public class StringRotation {
+    public static void main(String[] args) {
+        String s1 = "ABCD", s2 = "CDAB";
+        if (s1.length() == s2.length() && (s1 + s1).contains(s2))
+            System.out.println("Strings are rotations");
+        else
+            System.out.println("Not rotations");
+    }
+}
